dd5c4a0c6a2a7b47bde4dbb51bda5f053f323ec5
A0000000000 <mengxin_zzl@outlook.com>
2021-01-14 01:32:23 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EnterprisePortal-FrontEnd
-毕业设计的前端项目
+* 毕业设计的前端项目
+
+# 使用方法
+* 依赖安装: npm install
+* 运行程序: npm run serve
+* 打包程序: npm run build
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  width: 100%;
+  font-size: 20px;
+}
+</style>
